dc1daa2695e80b2ae543faa714c2405a6e67a6bb
Jack Turner <jackwilliamturner@icloud.com>
2018-02-15 16:52:01 +0000

    changing batchnorm layer to alter means update if follow-up to altered layer

--- bn.py
+++ bn.py
@@ -12,11 +12,11 @@ Batchnorm as a module - autograd takes care of gamma/beta
 Unsure how to add ISTA penalty to gamma update
 '''
 class BatchNorm2d(nn.Module):
-    def __init__(self, size, ista, stride=2):
+    def __init__(self, size, ista, follow=False, stride=2):
         super().__init__()
         self.beta  = nn.Parameter(torch.zeros(size, 1, 1))
         self.gamma = nn.Parameter(torch.ones(size, 1, 1))
-        self.ista  = ista # ista regularisation
+        self.follow = follow # used to decide on avg update strategy
 
     def forward(self, x):
         x_norm = x.transpose(0,1).contiguous().view(x.size(1), -1)
@@ -27,5 +27,9 @@ class BatchNorm2d(nn.Module):
 
         x = x - self.means
         x = x / self.stds
+
+        if follow:
+            # update means differently
+            self.means - <mask of 1s where gamma = 0> * relu(self.beta).t * sum_reduce(W)
 
         return self.gamma * x + self.beta
